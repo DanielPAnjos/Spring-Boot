99293fb96c001c75e7fa4e8910e7db5b6e3448ce
DanielPAnjos <pombo.daniel@gmail.com>
2022-01-20 17:12:29 -0300

    Utilizando Spring Security

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$w7XpgjIjxB2FASIBTj5yIOdW8DVJiyH8fDLBzVm9js5pmABJZNlq.');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('Java', 'Programação');
